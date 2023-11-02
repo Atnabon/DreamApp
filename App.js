baa9279ffea9e9727427c2cdecb8a8c1f2123c2c
atnabon <atnabond@gmail.com>
2023-11-02 10:40:56 +0300

    adding a separator

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  Button,
+  View,
+} from "react-native";
+
+const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
   console.log("app executed");
 
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <Text onPress={() => console.log("Text")}>Hello React Native</Text>
+        <Separator />
+        <Image
+          style={{ height: 100, width: 150 }}
+          source={{ uri: "https://ketari.vercel.app/ride.jpeg" }}
+        />
+        <Separator />
+        <Button
+          color="orange"
+          title="click me"
+          onPress={() => alert("button clicked")}
+        />
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -19,5 +42,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 8,
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: "#737373",
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
